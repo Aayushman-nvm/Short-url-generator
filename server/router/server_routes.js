41a0fbaaf93f33267ee746098b530806a66c2c04
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-11 22:42:13 +0530

    added history route

--- server/router/server_routes.js
+++ server/router/server_routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { handleUrlGenerator } from "../controller/urlGenerator.js";
+import { handleUrlGenerator, handleAllUrl } from "../controller/urlGenerator.js";
 import { handleLogin, handleRegister } from "../controller/userRelated.js";
 const router = Router();
 
 router.post("/url", handleUrlGenerator);
+router.post("/history", handleAllUrl);
 router.post("/login", handleLogin);
 router.post("/register", handleRegister);
 
