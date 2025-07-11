23e9ebc89f68c384ab50414ddcaec72914cd38ba
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-11 22:41:12 +0530

    Added get all url function

--- server/controller/urlGenerator.js
+++ server/controller/urlGenerator.js
@@ -4,17 +4,24 @@ import { URL } from "../model/url.js";
 export async function handleUrlGenerator(req, res) {
   const body = req.body;
   const shortId = nanoid(8);
+  console.log(req.body)
 
-  await URL.create({
+  const url=await URL.create({
+    createdBy:body.id,
     shortId: shortId,
     redirectUrl: body.url,
   });
 
-  const allUrls = await URL.find({});
+  const savedUrl = await url.save();
 
-  return res.json({
-    url: allUrls,
-    id: shortId,
-  });
+  return res.json(savedUrl);
+}
 
+export async function handleAllUrl(req, res){
+  const body=req.body;
+  console.log(body);
+  const allUrl= await URL.find({createdBy:body.id});
+  console.log(allUrl);
+
+  return res.json(allUrl);
 }
