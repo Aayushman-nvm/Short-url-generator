d809d18b4ab46893fc6d7fa18ea113c7994bd5d8
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-11 22:40:00 +0530

    Added history logic

--- client/src/Pages/History.jsx
+++ client/src/Pages/History.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
-
+import { useEffect,useState } from "react"
+import { useSelector } from "react-redux";
 function History() {
+  const userId=useSelector((state)=>state.user._id);
+  const [allUrl, setAllUrl]=useState();
+
+  async function getAllUrl() {
+    const response=await fetch("http://localhost:5000/history",{
+      method:"POST",
+      headers:{"content-type":"application/json"},
+      body:JSON.stringify({id:userId}),
+    });
+    const data= await response.json();
+    console.log(data);
+    setAllUrl(data);
+    return data;
+  }
+
+useEffect(()=>{
+  try {
+    getAllUrl();
+  } catch (error) {
+    console.log(error);
+  }
+},[]);
+
   return (
-    <div>History</div>
+    <div>History
+      {allUrl && <div>
+        {allUrl.map((item, index)=><p key={index}>id:{item.shortId} url:{item.redirectUrl}</p>)}
+        </div>}
+    </div>
   )
 }
 
