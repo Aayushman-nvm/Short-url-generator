d038c60eb370d9b8b4bc346767cd855c5f8e7ec5
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-16 20:33:58 +0530

    Can like or unlike from homepage

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react"
-import {useSelector} from "react-redux"
+import { useSelector } from "react-redux"
 function HomePage() {
   const [reqUrl, setReqUrl] = useState("");
-  const [data, setData]=useState(null);
-  const user=useSelector((state)=>state?.user);
-  const token=useSelector((state)=>state.token);
-  const userId=user._id;
+  const [data, setData] = useState(null);
+  const user = useSelector((state) => state?.user);
+  const token = useSelector((state) => state.token);
+  const userId = user._id;
 
   useEffect(() => {
     console.log(reqUrl);
@@ -16,27 +16,45 @@ function HomePage() {
       const response = await fetch("http://localhost:5000/url", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({url:reqUrl,id:userId}),
+        body: JSON.stringify({ url: reqUrl, id: userId }),
       });
-      const data= await response.json();
+      const data = await response.json();
       console.log("RESPONSE: ", data);
       console.log("BODY URL", data.redirectUrl);
       console.log("BODY ID", data.shortId);
-      console.log("ID: ",user+" Token: ",token);
+      console.log("ID: ", user + " Token: ", token);
       setData(data);
     } catch (error) {
       console.log(error);
     }
   }
+
+  async function handleLike() {
+    try {
+      const response= await fetch("http://localhost:5000/likes",{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({id:data._id,liked:data.liked}),
+      });
+      const result=await response.json();
+      console.log("Liked data: ", result);
+      setData(result);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div>HomePage
       <input onChange={(e) => setReqUrl(e.target.value)} value={reqUrl} placeholder="Enter your url here" />
       <button onClick={handleSubmit}>Submit</button>
-      {data && 
-      <div>
-        <a href={`https://${data.url}`}>http://shorturl/{data.id}</a>
+      {data &&
+        <div>
+          <a href={`https://${data.redirectUrl}`}>http://shorturl/{data.shortId}</a>
+          <button onClick={handleLike}>Like</button>
         </div>
-        }
+      }
     </div>
   )
 }
