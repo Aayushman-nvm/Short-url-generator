1b7c6f8420030da8bfc5988ae5d3c36d55398a00
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-16 20:35:01 +0530

    Likes, favourites and delete routes

--- server/router/server_routes.js
+++ server/router/server_routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { handleUrlGenerator, handleAllUrl } from "../controller/urlGenerator.js";
+import { handleUrlGenerator, handleAllUrl, handleLikedUrl, handleAllLikedUrl, handleDeleteUrl } from "../controller/urlGenerator.js";
 import { handleLogin, handleRegister } from "../controller/userRelated.js";
 const router = Router();
 
 router.post("/url", handleUrlGenerator);
 router.post("/history", handleAllUrl);
+router.post("/likes", handleLikedUrl);
+router.post("/favourites", handleAllLikedUrl);
+router.post("/delete",handleDeleteUrl);
 router.post("/login", handleLogin);
 router.post("/register", handleRegister);
 
