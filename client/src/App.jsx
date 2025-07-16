1f0b8ab1f71479edf07a972bb0670fb0689ccda1
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-16 21:14:51 +0530

    Styled app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,28 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar";
-import Footer from './Components/Footer'
+import Footer from "./Components/Footer";
 import HomePage from "./Pages/HomePage";
-import History from "./Pages/History"
+import History from "./Pages/History";
 import Favourites from "./Pages/Favourites";
 import Login from "./Pages/Login";
 
 function App() {
-
   return (
-    <div>Hello
+    <div className="min-h-screen flex flex-col bg-black text-white font-sans">
       <BrowserRouter>
         <NavBar />
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/favourites" element={<Favourites />} />
-        </Routes>
+        <main className="flex-grow container mx-auto px-4 py-6">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/favourites" element={<Favourites />} />
+          </Routes>
+        </main>
         <Footer />
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
