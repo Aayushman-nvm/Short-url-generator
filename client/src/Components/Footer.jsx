278a6a1f62c730fba64ee4470f29470bc533b853
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-17 00:57:44 +0530

    styled footer

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -2,25 +2,39 @@ import { Link } from "react-router-dom";
 import { Github, Twitter } from "lucide-react";
 
 function Footer() {
+
   return (
-    <footer className="w-full flex flex-col sm:flex-row justify-between items-center px-4 py-4 bg-gradient-to-r from-black via-gray-900 to-orange-900 text-white mt-8">
-      <div className="text-sm">© 2025 SunsetApp. All rights reserved.</div>
-      <div className="flex space-x-4 mt-2 sm:mt-0">
-        <Link to="/home" className="hover:text-orange-400 transition">Home</Link>
-        <Link to="/history" className="hover:text-orange-400 transition">History</Link>
-        <Link to="/favourites" className="hover:text-orange-400 transition">Favourites</Link>
-        <Link to="/" className="hover:text-orange-400 transition">Login/SignUp</Link>
+    <footer className="w-full mt-12 pt-6 px-7 bg-black text-white">
+      <div className="flex border-t border-gray-700 flex-row justify-between items-start sm:items-center w-full">
+        {/* Navigation */}
+        <div className="flex flex-col space-y-2 my-4">
+          <Link to="/home" className="hover:text-orange-400 transition">Home</Link>
+          <Link to="/history" className="hover:text-orange-400 transition">History</Link>
+          <Link to="/favourites" className="hover:text-orange-400 transition">Favourites</Link>
+        </div>
+
+        {/* Social Links */}
+        <div className="flex flex-col items-center mt-6 sm:mt-0">
+          <span className="text-sm mb-2 text-gray-400">Social Links</span>
+          <div className="flex space-x-3 justify-center">
+            <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="hover:text-orange-400 transition">
+              <Github size={20} />
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="hover:text-orange-400 transition">
+              <Twitter size={20} />
+            </a>
+          </div>
+        </div>
+
       </div>
-      <div className="flex space-x-3 mt-2 sm:mt-0">
-        <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="hover:text-orange-400 transition">
-          <Github size={20} />
-        </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="hover:text-orange-400 transition">
-          <Twitter size={20} />
-        </a>
+
+      {/* Copyright */}
+      <div className="text-center text-sm text-gray-400 py-2 border-t border-gray-800">
+        © 2025 Short Url. All rights reserved.
       </div>
     </footer>
   );
+
 }
 
 export default Footer;
