6763733d92595d2e85862499809108559c4a84ce
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-16 20:34:39 +0530

    handle all urls, liked urls, all liked urls and delete urls

--- server/controller/urlGenerator.js
+++ server/controller/urlGenerator.js
@@ -4,12 +4,13 @@ import { URL } from "../model/url.js";
 export async function handleUrlGenerator(req, res) {
   const body = req.body;
   const shortId = nanoid(8);
-  console.log(req.body)
+  console.log(req.body);
 
-  const url=await URL.create({
-    createdBy:body.id,
+  const url = await URL.create({
+    createdBy: body.id,
     shortId: shortId,
     redirectUrl: body.url,
+    liked: false,
   });
 
   const savedUrl = await url.save();
@@ -17,11 +18,42 @@ export async function handleUrlGenerator(req, res) {
   return res.json(savedUrl);
 }
 
-export async function handleAllUrl(req, res){
-  const body=req.body;
+export async function handleAllUrl(req, res) {
+  const body = req.body;
   console.log(body);
-  const allUrl= await URL.find({createdBy:body.id});
+  const allUrl = await URL.find({ createdBy: body.id });
   console.log(allUrl);
 
   return res.json(allUrl);
+}
+
+export async function handleLikedUrl(req, res) {
+  console.log("Liked: ", req.body);
+  const body = req.body;
+  const updateliked = !body.liked;
+  console.log("After change: ", updateliked);
+  const updatedUrl = await URL.findByIdAndUpdate(
+    body.id,
+    { liked: updateliked },
+    { new: true }
+  );
+  console.log("Url after liking: ",updatedUrl);
+  return res.json(updatedUrl);
+}
+
+export async function handleAllLikedUrl(req, res) {
+  const body = req.body;
+  console.log(body);
+  const allUrl = await URL.find({ createdBy: body.id, liked: true });
+  console.log("All liked url: ",allUrl);
+
+  return res.json(allUrl);
+}
+
+export async function handleDeleteUrl(req, res) {
+  const body=req.body;
+  console.log("Backend body: ",body);
+  const deletedUrl=await URL.findByIdAndDelete(body.id);
+  const allUrl=await URL.find({ createdBy: body.userId });
+  return res.json({deletedUrl:deletedUrl, allUrl:allUrl});
 }
