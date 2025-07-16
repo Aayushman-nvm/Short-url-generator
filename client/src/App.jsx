3900b46f0b83837cdaf5c3fd870612d6577ca773
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-16 23:49:31 +0530

    Added auth check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
 import HomePage from "./Pages/HomePage";
@@ -7,6 +8,8 @@ import Favourites from "./Pages/Favourites";
 import Login from "./Pages/Login";
 
 function App() {
+  const token = useSelector((state) => state.token);
+
   return (
     <div className="min-h-screen flex flex-col bg-black text-white font-sans">
       <BrowserRouter>
@@ -14,9 +17,9 @@ function App() {
         <main className="flex-grow container mx-auto px-4 py-6">
           <Routes>
             <Route path="/" element={<Login />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/history" element={<History />} />
-            <Route path="/favourites" element={<Favourites />} />
+            <Route path="/home" element={token ? <HomePage /> : <Navigate to={"/"} />} />
+            <Route path="/history" element={token ? <History /> : <Navigate to={"/"} />} />
+            <Route path="/favourites" element={token ? <Favourites /> : <Navigate to={"/"} />} />
           </Routes>
         </main>
         <Footer />
